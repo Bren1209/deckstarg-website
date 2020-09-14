27facfa6892e9856a88aa068e53dfff0be120a1f
Bren1209 <bs4metal@gmail.com>
2020-09-14 12:26:33 +0200

    add login/signup forms and configure backend for registration/logon

--- app.js
+++ app.js
@@ -1,15 +1,75 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
 
 const app = express();
-
-app.set('view engine', 'ejs');
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: true}))
+app.set('view engine', 'ejs')
+
+mongoose.connect('mongodb://localhost:27017/deckstarUserDB', {useNewUrlParser: true, useUnifiedTopology: true})
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    password: String
+})
+
+const secret = process.env.SECRET;
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']});
+
+const User = new mongoose.model('User', userSchema)
 
 app.get('/', (req, res) => {
     res.render('index');
 })
 
-app.listen(process.env.PORT, process.env.IP, () => {
+app.get('/login', (req, res) => {
+    res.render('login');
+})
+
+app.get('/signup', (req, res) => {
+    res.render('signup');
+})
+
+app.post('/signup', function(req, res){
+    const username = req.body.username
+    const email = req.body.email
+    const password = req.body.password
+
+    const newUser = new User ({
+        username: username,
+        email: email,
+        password: password
+    });
+    newUser.save(function(err){
+        if(err){
+            console.log(err);
+        } else {
+            res.render('login');
+        }
+    });
+});
+
+app.post('/login', function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({username: username}, function(err, foundUser){
+        if(err) {
+            console.log(err)
+        } else {
+            if(foundUser){
+                if(foundUser.password === password) {
+                    res.render('index')
+                }
+            }
+        }
+    })
+})
+
+app.listen(process.env.PORT || 8080, process.env.IP, () => {
     console.log('Server running')
 })
